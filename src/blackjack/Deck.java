608a3c19bc4cd99ade543472ed4e2470dfa4e54a
Nirdesh Shrestha <sthanirdesh25@gmail.com>
2020-05-07 15:02:50 -0400

    Building a deck 1st commit

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -2,6 +2,36 @@ package blackjack;
 
 public class Deck {
 	public static void main(String[] args) {
-		System.out.println("print something");
+		 String[] SUITS = {
+		            "Clubs", "Diamonds", "Hearts", "Spades"
+		        };
+
+		        String[] RANKS = {
+		            "2", "3", "4", "5", "6", "7", "8", "9", "10",
+		            "Jack", "Queen", "King", "Ace"
+		        };
+
+		        // initialize deck
+		        int n = SUITS.length * RANKS.length;
+		        String[] deck = new String[n];
+		        for (int i = 0; i < RANKS.length; i++) {
+		            for (int j = 0; j < SUITS.length; j++) {
+		                deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
+		                System.out.println(deck[i]);
+		            }
+		            
+		        }
+		        // shuffle
+		        for (int i = 0; i < n; i++) {
+		            int r = i + (int) (Math.random() * (n-i));
+		            String temp = deck[r];
+		            deck[r] = deck[i];
+		            deck[i] = temp;
+		        }
+
+		        // print shuffled deck
+		        for (int i = 0; i < n; i++) {
+		            System.out.printf("Card #%d is %s\n", i+1, deck[i]);
+		        }
 	}
 }
