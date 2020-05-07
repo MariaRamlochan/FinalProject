62e74de7f4bf458b24f798b54b8fea956e1aeac1
Nirdesh Shrestha <sthanirdesh25@gmail.com>
2020-05-07 16:23:11 -0400

    Building Deck Part 2

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -1,37 +1,40 @@
 package blackjack;
 
 public class Deck {
-	public static void main(String[] args) {
-		 String[] SUITS = {
-		            "Clubs", "Diamonds", "Hearts", "Spades"
-		        };
+    public static void main(String[] args) {
+        String[] Suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] Ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
-		        String[] RANKS = {
-		            "2", "3", "4", "5", "6", "7", "8", "9", "10",
-		            "Jack", "Queen", "King", "Ace"
-		        };
+        // initialize deck
+        int totalCards = Suits.length * Ranks.length;
+        String[] deck = new String[totalCards];
+        for (int i = 0; i < Ranks.length; i++) {
+            for (int j = 0; j < Suits.length; j++) {
+                deck[Suits.length*i + j] = Ranks[i] + " of " + Suits[j];
+            }
+        }
+        
+        for (int i = 0; i < totalCards; i++) {
+            String temp = deck[i];
+            deck[i] = temp;
+        }
 
-		        // initialize deck
-		        int n = SUITS.length * RANKS.length;
-		        String[] deck = new String[n];
-		        for (int i = 0; i < RANKS.length; i++) {
-		            for (int j = 0; j < SUITS.length; j++) {
-		                deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
-		                System.out.println(deck[i]);
-		            }
-		            
-		        }
-		        // shuffle
-		        for (int i = 0; i < n; i++) {
-		            int r = i + (int) (Math.random() * (n-i));
-		            String temp = deck[r];
-		            deck[r] = deck[i];
-		            deck[i] = temp;
-		        }
+        // print initial deck
+        for (int i = 0; i < totalCards; i++) {
+            System.out.println(deck[i]);
+        }
+        
+        // shuffle
+        for (int i = 0; i < totalCards; i++) {
+            int r = i + (int) (Math.random() * (totalCards-i));
+            String temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
 
-		        // print shuffled deck
-		        for (int i = 0; i < n; i++) {
-		            System.out.printf("Card #%d is %s\n", i+1, deck[i]);
-		        }
-	}
+        // print shuffled deck
+        for (int i = 0; i < totalCards; i++) {
+            System.out.println(deck[i]);
+        }
+    }
 }
