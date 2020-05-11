47d8665d36b2dcd1a86e2553a70292873fac3111
Nirdesh Shrestha <sthanirdesh25@gmail.com>
2020-05-11 16:55:18 -0400

    some changes

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -15,25 +15,6 @@ public class BlackJack {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-<<<<<<< Upstream, based on branch 'master' of https://github.com/MariaRamlochan/FinalProject.git
-        String name;
-        //int cash = Players.getCash(); //Player's initial amount.
-        int bet; //Amount the player will bet.
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("******** WELCOME TO BLACKJACK *********");
-        
-        System.out.println("Plese enter a Username: ");
-        name = input.nextLine();
-        Players player = new Players(name);
-        player.setName(name);
-        player.toString();
-        
-        
-        System.out.println("\nEnter the anount you wish bet: ");
-        bet = input.nextInt();
-=======
 //        String name;
 //        int cash = Players.getCash(); //Player's initial amount.
 //        int bet; //Amount the player will bet.
@@ -53,7 +34,6 @@ public class BlackJack {
     	
     	Deck deck = new Deck();
     	deck.shuffledDeck();
->>>>>>> 15e87a3 perfecting the Deck class
     }
     
     
