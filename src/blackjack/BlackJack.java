8cfc54e8b908f2de029034e7d767b471868a5aee
Nirdesh Shrestha <sthanirdesh25@gmail.com>
2020-05-11 22:41:50 -0400

    Game class ep 1 part 1

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -32,22 +32,7 @@ public class BlackJack {
         System.out.println("\nEnter the amount you wish bet: ");
         bet = input.nextInt();
     	
-/*
-    	// testing if the deck prints
-    	Deck deck = new Deck();
-    	deck.shuffledDeck();
-    	int r;
-    	for(int initialCards = 0; initialCards < 4; initialCards++) {
-    		r = initialCards % 2;
-    		if(r == 1) {
-    			System.out.println(initialCards + " is odd");
-    			System.out.println("dealer's hand: " + deck.deck[initialCards]);
-    		} else {
-    			System.out.println(initialCards + " is even");
-    			System.out.println("players's hand: " + deck.deck[initialCards]);
-    		}    		
-    	}
-*/
+
     }
     
     
