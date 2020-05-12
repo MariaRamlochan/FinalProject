16df7f06f0fbfd2d2c99e9740595373b37e49247
Nirdesh Shrestha <sthanirdesh25@gmail.com>
2020-05-11 20:19:11 -0400

    Testing 1

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -33,17 +33,17 @@ public class BlackJack {
 //        bet = input.nextInt();
     	
     	// testing if the deck prints
-    	Deck game = new Deck();
-    	game.shuffledDeck();
+    	Deck deck = new Deck();
+    	deck.shuffledDeck();
     	int r;
     	for(int initialCards = 0; initialCards < 4; initialCards++) {
     		r = initialCards % 2;
     		if(r == 1) {
     			System.out.println(initialCards + " is odd");
-    			System.out.println("dealer's hand: " + game.deck[initialCards]);
+    			System.out.println("dealer's hand: " + deck.deck[initialCards]);
     		} else {
     			System.out.println(initialCards + " is even");
-    			System.out.println("players's hand: " + game.deck[initialCards]);
+    			System.out.println("players's hand: " + deck.deck[initialCards]);
     		}    		
     	}
     }
