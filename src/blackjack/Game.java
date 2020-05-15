d251b56426986dfd590fd49484e7ef9c65a77e3d
MariaRamlochan <mariaramlochan@hotmail.com>
2020-05-15 12:44:19 -0400

    Added other lines and placed clear comments

--- src/blackjack/Game.java
+++ src/blackjack/Game.java
@@ -1,19 +1,22 @@
 package blackjack;
-
+/**
+ *
+ * @author Maria Ramlochan & Nirdesh Shrestha
+ */
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
 	
-	public static String[] dealerHand = new String[2];
-	public static String[] playerHand = new String[2];
+    public static String[] dealerHand = new String[2];
+    public static String[] playerHand = new String[2];
 	
     public static void initialCards(){
-        // testing if the deck prints
         
-    	Deck deck1 = new Deck();
+        //Initialising the deck class.
+        Deck deck1 = new Deck();
         Deck.shuffledDeck();
-    	
+        
         int r;
         
         // To calculate the number of cards in the players and dealers have 
@@ -22,20 +25,22 @@ public class Game {
         System.out.println(Arrays.toString(Deck.deck));
         for(int initialCards = 0; initialCards < 4; initialCards++) {
             
-			r = initialCards % 2;
+            r = initialCards % 2;
             if(r == 1) {
+                //Placing the 2nd and 4th card in the dealer's hand.
             	System.out.println("Dealer draws: " + Deck.deck[initialCards]);
-                
                 dealerHand[dealerCards] = Deck.deck[initialCards];
+                
                 //printing the first words of the card
                 int index = dealerHand[dealerCards].indexOf(' ');
                 String card = dealerHand[dealerCards].substring(0, index);
                 
                 //System.out.println(card);
                 Values.values(card);
-                
                 dealerCards++;
-            } else { 
+                
+            } else {
+                //Placing the 1st and 3rd card in the players hands.
             	System.out.println("Player draws: " + Deck.deck[initialCards]);
             	
             	playerHand[playerCards] = Deck.deck[initialCards]; 
@@ -53,22 +58,24 @@ public class Game {
         Scanner sc = new Scanner(System.in);
         System.out.println("Do you want to Hit or Stand?\nType 'hit' to Hit and 'stand' to Stand");
         String nextMove = sc.nextLine();
-        if (nextMove.equalsIgnoreCase("hit")) {
-			//System.out.println("you get the next card");
-        	String card = Deck.deck[4];
-        	
-        	int p = 2;
-        	String[] playerHand2 = Arrays.copyOf(playerHand, playerHand.length+1);;	
-        	playerHand2[2] = card;
-        	System.out.println(card);
-        	
-        	System.out.println(Arrays.toString(playerHand2));
-        	
-		} else if (nextMove.equalsIgnoreCase("stand")) {
-			//System.out.println("you selected to stand");
-		}
         
-        //Printout the Player's and Dealer's hand while showing the space available
+        if (nextMove.equalsIgnoreCase("hit")) {
+            //System.out.println("you get the next card");
+            String card = Deck.deck[4];
+        	
+        //Increasing the lenght in which the player hand can hold the next card.        
+        	int p = 2;
+        	String[] playerHand2 = Arrays.copyOf(playerHand, playerHand.length+1);	
+        	playerHand2[2] = card;
+        	System.out.println("You draw: " + card);
+        	
+        	System.out.println("Your hand: " + Arrays.toString(playerHand2));
+        	
+        } else if (nextMove.equalsIgnoreCase("stand")) {
+	//System.out.println("you selected to stand");
+	}
+        
+//        //Printout the Player's and Dealer's hand while showing the space available
 //        System.out.println("****************************************************");
 //        System.out.println("\nYour hand: " + Arrays.toString(playerHand));
 //        System.out.println("Player's cards value: ");
