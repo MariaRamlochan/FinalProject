8b2ee50fd2a21ac6fa3031aa8203adbf2c1547cc
Nirdesh Shrestha <sthanirdesh25@gmail.com>
2020-05-15 16:30:56 -0400

    Problem with do while loop

--- src/blackjack/Game.java
+++ src/blackjack/Game.java
@@ -13,104 +13,102 @@ public class Game {
 	
     public static void initialCards(){
         
-        //Initialising the deck class.
+        //Initializing the deck class.
         Deck deck1 = new Deck();
         Deck.shuffledDeck();
         
         int r;
         
         // To calculate the number of cards in the players and dealers have 
-        int dealerCards = 0;
-        int playerCards = 0;
+        int dealerCards = 0, playerCards = 0;
+        int playerValue = 0, dealerValue = 0;
         System.out.println(Arrays.toString(Deck.deck));
         for(int initialCards = 0; initialCards < 4; initialCards++) {
             
             r = initialCards % 2;
-            if(r == 1) {
-                //Placing the 2nd and 4th card in the dealer's hand.
-            	System.out.println("Dealer draws: " + Deck.deck[initialCards]);
-                dealerHand[dealerCards] = Deck.deck[initialCards];
-                
-                //printing the first words of the card
-                int index = dealerHand[dealerCards].indexOf(' ');
-                String card = dealerHand[dealerCards].substring(0, index);
-                
-                //System.out.println(card);
-                Values.values(card);
-                dealerCards++;
-                
-            } else {
-                //Placing the 1st and 3rd card in the players hands.
-            	System.out.println("Player draws: " + Deck.deck[initialCards]);
+            if(r==0) {
+            	 //Placing the 1st and 3rd card in the players hands.
+            	System.out.print("Draw " + (playerCards + 1) + ":\nPlayer draws: " + Deck.deck[initialCards]);
             	
             	playerHand[playerCards] = Deck.deck[initialCards]; 
             	
-                //printing the first words of the card
-                int index = playerHand[playerCards].indexOf(' ');
-                String card = playerHand[playerCards].substring(0, index);
+            	String card = playerHand[playerCards];
                 
-                //System.out.println(card);
-                Values.values(card);
-                
+                playerValue += Values.values(card);
+                //System.out.println("Player has: " + playerValue);
                 playerCards++;
-             	}   	
-        	}
+            } else {
+            	//Placing the 2nd and 4th card in the dealer's hand.
+            	System.out.println("\t| Dealer draws: " + Deck.deck[initialCards]+ "\n");
+                dealerHand[dealerCards] = Deck.deck[initialCards];
+                
+                String card = dealerHand[dealerCards];
+                
+                dealerValue += Values.values(card);
+                //System.out.println("Dealer has: " + dealerValue);
+                dealerCards++;
+            }  
+        }
+      
         Scanner sc = new Scanner(System.in);
         System.out.println("Do you want to Hit or Stand?\nType 'hit' to Hit and 'stand' to Stand");
-        String nextMove = sc.nextLine();
-
-        if (nextMove.equalsIgnoreCase("hit")) {
-			//System.out.println("you get the next card");
-        	String card = Deck.deck[4];
-        	
-        	int p = 2;
-        	
-        	String[] playerHand2 = Arrays.copyOf(playerHand, playerHand.length+1);;	
-        	playerHand2[2] = card;
-        	System.out.println(card);
-        	
-        	System.out.println(Arrays.toString(playerHand2));
-        	
-		} else if (nextMove.equalsIgnoreCase("stand")) {
-			//System.out.println("you selected to stand");
-		}
+        String nextMove;
+        int nextCard = 4; //the location of the next card in the deck
+        String[] playerHand2 = Arrays.copyOf(playerHand, playerHand.length + 1);
+        do{ 
+        	nextMove = sc.nextLine();
+        	if(playerValue == 21) {
+        		System.out.println("You Won!");
+        	} else if (nextMove.equalsIgnoreCase("hit") && playerValue < 21){
+	        	String card = Deck.deck[nextCard];
+	        	
+	            //Increasing the length in which the player hand can hold the next card.        
+	        	playerHand2[playerCards] = card;
+	        	
+	        	System.out.println("You draw: " + card);
+	        	
+	        	//Coping and increasing one space in players hand
+	        	playerHand = Arrays.copyOf(playerHand2, playerHand2.length);
+	        	playerHand2 = Arrays.copyOf(playerHand, playerHand.length+1);
+	        	playerValue += Values.values(card);
+	        	System.out.println(playerValue);
+	        	playerCards++;
+	        	System.out.println("Your hand: " + Arrays.toString(playerHand));
+	        	
+	        	nextCard++; //incrementing the nextCard
+	        	
+	        	//System.out.println(nextCard);testing if nextCard works	
+	        } else if (nextMove.equalsIgnoreCase("stand")) {
+	        	System.out.println("you selected to stand");
+	        	String[] dealerHand2 = Arrays.copyOf(dealerHand, dealerHand.length + 1);
+	        	while(dealerValue < playerValue || dealerValue <18){
+	        		String card = Deck.deck[nextCard];
+	        		dealerHand2[dealerCards] = card;
+	        		
+	       			System.out.println("Dealer Draws: " + card);
+	        			
+	       			dealerHand = Arrays.copyOf(dealerHand2, dealerHand2.length);
+	   	        	dealerHand2 = Arrays.copyOf(dealerHand, dealerHand.length+1);
+	            	
+	   	        	dealerValue += Values.values(card);
+	   	        	dealerCards++;
+	    	        	
+	   	        	System.out.println(dealerValue);
+    	        	System.out.println("Your hand: " + Arrays.toString(dealerHand));
+	        	} 
+	        } else {
+	        	System.out.println("You are busted");
+	        }
+	        
+        } while(nextMove.equalsIgnoreCase("stand") && playerCards<5);    
+     } 
         
-        if (nextMove.equalsIgnoreCase("hit")) {
-            //System.out.println("you get the next card");
-            String card = Deck.deck[4];
-        	
-        //Increasing the lenght in which the player hand can hold the next card.        
-        	int p = 2;
-        	String[] playerHand2 = Arrays.copyOf(playerHand, playerHand.length+1);	
-        	playerHand2[2] = card;
-        	System.out.println("You draw: " + card);
-        	
-        	System.out.println("Your hand: " + Arrays.toString(playerHand2));
-        	
-        } else if (nextMove.equalsIgnoreCase("stand")) {
-	//System.out.println("you selected to stand");
-	}
-        
-//        //Printout the Player's and Dealer's hand while showing the space available
+//        Printout the Player's and Dealer's hand while showing the space available
 //        System.out.println("****************************************************");
 //        System.out.println("\nYour hand: " + Arrays.toString(playerHand));
 //        System.out.println("Player's cards value: ");
 //        System.out.println("****************************************************");
 //        System.out.println("\nDealer's hand: " + Arrays.toString(dealerHand));
-//        System.out.println("Dealer's cards value: ");
-        
-    }
-    
-    //This was the constructor for the next move
-//    public static void nextMove(String nextMove) {
-//		Deck.deck;
-//    	if (nextMove.equalsIgnoreCase("hit")) {
-//			//System.out.println("you get the next card");
-//			playerHand = Deck.shuffledDeck();
-//			System.out.println(playerHand);
-//		} else if (nextMove.equalsIgnoreCase("stand")) {
-//			//System.out.println("you selected to stand");
-//		}
-//
-//	}
+//        System.out.println("Dealer's cards value: "); 
 }
+
--- src/blackjack/Values.java
+++ src/blackjack/Values.java
@@ -14,66 +14,61 @@ public class Values {
         public static int values(String card1) {
         int value = 0;
         String card = card1;
+        int index = card.indexOf(' ');
+        card = card.substring(0, index);
         	switch(card) {
             	case "Ace": 
             		value = 1;
-            		System.out.println(value);
+            		//System.out.println(value);
             		break;
             	case "2": 
             		value = 2;
-            		System.out.println(value);
+            		//System.out.println(value);
             		break;
 	            case "3": 
 	            	value = 3;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "4": 
 	            	value = 4;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "5": 
 	            	value = 5;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "6": 
 	            	value = 6;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "7": 
 	            	value = 7;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "8": 
 	            	value = 8;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "9": 
 	            	value = 9;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "10": 
 	            	value = 10;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "Jack":
 	            	value = 10;
-	                System.out.println(value);
+	                //System.out.println(value);
 	            	break;
 	            case "Queen": 
 	            	value = 10;
-	                System.out.println(value);
+	                //System.out.println(value);
 	                break;
 	            case "King": 
 	            	value = 10;
-	                System.out.println(value);
+	                //System.out.println(value);
 	                break;  
-	            default:
-	            	value = 0;
-	            	System.out.println("hello");
-	    }
-        
-        return value;
-        
+	    }  return value;    
     } 
-    
 }
