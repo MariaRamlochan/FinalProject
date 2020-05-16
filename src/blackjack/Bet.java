33f9974ae1180e5cd6de11008e6b768a72f5c7fc
Nirdesh Shrestha <sthanirdesh25@gmail.com>
2020-05-16 01:12:00 -0400

    Bet class Finialized

--- src/blackjack/Bet.java
+++ src/blackjack/Bet.java
@@ -3,11 +3,16 @@ package blackjack;
 public class Bet {
 	public static int chips = 2000; //Player's initial amount.
 	public static int bet;
-	
+	/**
+	 * Bet Constructor
+	 * @param b 		Amount of bet
+	 * @param gameW		Game Won/Lost/Draw
+	 */
 	public Bet(int b, String gameW) {
-		bet = b;
+		bet = b; 
 		String gameWon = gameW;
 		this.chips = chips;
+		
 		if(gameWon.equalsIgnoreCase("true")) {
 			Bet.chips += bet;	
 			setChips(chips);
@@ -18,5 +23,9 @@ public class Bet {
 			setChips(chips);
 		}
 	} 
+	/**
+	 * Setter to set value of chips
+	 * @param chips
+	 */
 	public void setChips(int chips) { this.chips = chips; }
 }
